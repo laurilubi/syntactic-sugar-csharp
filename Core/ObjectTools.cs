dbff73cde5cb1b5803395a6984c620158a2c6155
Lauri Lubi <lauri@lubi.se>
2022-10-07 14:37:02 +0200

    ObjectTools

---
+++ Core/ObjectTools.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SyntacticSugar.Core;
+
+public static class ObjectTools
+{
+    public static bool IsOneOf<TSource>(this TSource item, params TSource[] values) =>
+        item.IsOneOf(values.ToList());
+
+    public static bool IsOneOf<TSource>(this TSource item, IEnumerable<TSource> values) =>
+        item.IsOneOf(values.ToList());
+
+    public static bool IsOneOf<TSource>(this TSource item, List<TSource> values)
+    {
+        return values.Contains(item);
+    }
+}
