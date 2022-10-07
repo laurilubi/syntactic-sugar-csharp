00231a8b4638ea38442d36c1226c3d2e4bac4e1d
Lauri Lubi <lauri@lubi.se>
2022-10-07 15:10:09 +0200

    BoolTools

---
+++ Core/SimpleValues/BoolTools.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using SyntacticSugar.Core;
+using SyntacticSugar.Core.Exceptions;
+using SyntacticSugar.Core.SimpleValues;
+
+namespace Core.Tools;
+
+public static class BoolTools
+{
+    public static bool GetBoolOrDefault(object input, bool defaultValue = false)
+    {
+        if (input == null) return defaultValue;
+        var value = GetBoolOrNull(input);
+        return value ?? defaultValue;
+    }
+
+    public static bool? GetBoolOrNull(object input)
+    {
+        if (input == null) return null;
+        if (input is string)
+        {
+            var inputString = input.ToString().ToLower();
+            if (inputString.IsOneOf("y", "yes", "1"))
+                input = "true";
+            else if (inputString.IsOneOf("n", "no", "0"))
+                input = "false";
+        }
+        else if (input is int inputInt)
+        {
+            if (inputInt == 1)
+                input = "true";
+            else if (inputInt == 0)
+                input = "false";
+        }
+
+        var success = bool.TryParse(input.ToString(), out var value);
+        return success
+            ? value
+            : null;
+    }
+
+    public static bool GetBoolOrThrow(object input) => GetBoolOrThrow(input,
+        input => new ModelValidationException($"Value {input} cannot be parsed as bool."));
+
+    public static bool GetBoolOrThrow<TException>(object input, Func<object, TException> createException)
+        where TException : Exception
+    {
+        var value = GetBoolOrNull(input);
+        AssertTools.Assert(value != null, () => createException(input));
+        return value.Value;
+    }
+}
--- Core/SimpleValues/ObjectTools.cs
+++ Core/SimpleValues/ObjectTools.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace SyntacticSugar.Core;
+namespace SyntacticSugar.Core.SimpleValues;
 
 public static class ObjectTools
 {
