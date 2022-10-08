2777458c16886a85c1a8175b55b1e474e9fefd9e
Lauri Lubi <lauri@lubi.se>
2022-10-08 16:26:22 +0200

    fix ListTools

--- Core/Collections/ListTools.cs
+++ Core/Collections/ListTools.cs
@@ -6,21 +6,18 @@ namespace Syntactic.Sugar.Core.Collections;
 
 public static class ListTools
 {
-    public static void AddOrSet<TValue>(this List<TValue> items, TValue value, Func<TValue, bool> selector)
+    public static void AddOrSet<TValue>(this IList<TValue> items, TValue value, Func<TValue, bool> selector)
     {
-        lock (items)
-        {
-            var indexes = items
-                .Where(selector)
-                .Select((item, index) => index)
-                .ToList();
-            AssertTools.Assert(indexes.Count < 2,
-                () => $"Sequence contains {indexes.Count} matches, but 0 or 1 expected.");
+        var indexes = items
+            .Where(selector)
+            .Select((item, index) => index)
+            .ToList();
+        AssertTools.Assert(indexes.Count < 2,
+            () => $"Sequence contains {indexes.Count} matches, but 0 or 1 expected.");
 
-            if (indexes.Count == 0)
-                items.Add(value);
-            else
-                items[indexes[0]] = value;
-        }
+        if (indexes.Count == 0)
+            items.Add(value);
+        else
+            items[indexes[0]] = value;
     }
 }
