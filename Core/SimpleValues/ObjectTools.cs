fc98e735f485ae0d67ccba097ec7fec1144fa243
Lauri Lubi <lauri@lubi.se>
2022-10-08 14:37:44 +0200

    IsBetween

--- Core/SimpleValues/ObjectTools.cs
+++ Core/SimpleValues/ObjectTools.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SyntacticSugar.Core.SimpleValues;
@@ -14,5 +15,16 @@ public static class ObjectTools
     public static bool IsOneOf<TSource>(this TSource item, List<TSource> values)
     {
         return values.Contains(item);
+    }
+
+    public static bool IsBetween<T>(this T value, T begin, T end, bool inclusive = true)
+        where T : IComparable<T>
+    {
+        AssertTools.Assert(begin != null, () => new ArgumentNullException(nameof(begin)));
+        AssertTools.Assert(end != null, () => new ArgumentNullException(nameof(end)));
+
+        return inclusive
+            ? begin.CompareTo(value) <= 0 && end.CompareTo(value) >= 0
+            : begin.CompareTo(value) < 0 && end.CompareTo(value) > 0;
     }
 }
