c911b0a469beb43115d8b69adc976ab138601d35
Lauri Lubi <lauri@lubi.se>
2022-10-08 16:39:30 +0200

    SplitStrings

--- Core/Collections/ListTools.cs
+++ Core/Collections/ListTools.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Syntactic.Sugar.Core.SimpleValues;
 
 namespace Syntactic.Sugar.Core.Collections;
 
@@ -19,5 +20,29 @@ public static class ListTools
             items.Add(value);
         else
             items[indexes[0]] = value;
+    }
+
+    public static List<int> SplitInts(this string values, char separator = ',')
+    {
+        return SplitStrings(values, separator: separator)
+            .Select(IntTools.GetIntOrThrow)
+            .ToList();
+    }
+
+    public static List<long> SplitLongs(this string values, char separator = ',')
+    {
+        return SplitStrings(values, separator: separator)
+            .Select(LongTools.GetLongOrThrow)
+            .ToList();
+    }
+
+    public static List<string> SplitStrings(this string values, char separator = ',', bool trimItem = true)
+    {
+        if (string.IsNullOrWhiteSpace(values)) return new List<string>();
+        return values
+            .Split(separator)
+            .Where(x => string.IsNullOrWhiteSpace(x) == false)
+            .Select(x => trimItem ? x.Trim() : x)
+            .ToList();
     }
 }
