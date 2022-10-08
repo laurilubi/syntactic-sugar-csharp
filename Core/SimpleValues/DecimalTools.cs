0ab3dbc2bae6cb46a334ff32c41945fbc76c6c37
Lauri Lubi <lauri@lubi.se>
2022-10-08 15:26:30 +0200

    DecimalTools

---
+++ Core/SimpleValues/DecimalTools.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Syntactic.Sugar.Core.Exceptions;
+
+namespace Syntactic.Sugar.Core.SimpleValues;
+
+public static class DecimalTools
+{
+    public static decimal? GetDecimalOrNull(object input)
+    {
+        if (input == null) return null;
+        if (input is decimal inputDecimal) return inputDecimal;
+        var success = decimal.TryParse(
+            input.ToString().Replace(',', '.'),
+            NumberStyles.Number,
+            NumberFormatInfo.InvariantInfo, 
+            out var value);
+        return success
+            ? value
+            : null;
+    }
+
+    public static decimal GetDecimalOrDefault(object input, decimal defaultValue = default)
+    {
+        var value = GetDecimalOrNull(input);
+        return value ?? defaultValue;
+    }
+
+    public static decimal GetDecimalOrThrow(object input) => GetDecimalOrThrow(input,
+        input => new ModelValidationException($"Value {input} cannot be parsed as decimal."));
+
+    public static decimal GetDecimalOrThrow<TException>(object input, Func<object, TException> createException)
+        where TException : Exception
+    {
+        var value = GetDecimalOrNull(input);
+        AssertTools.Assert(value != null, () => createException(input));
+        return value.Value;
+    }
+}
