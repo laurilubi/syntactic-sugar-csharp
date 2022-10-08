3015eda55ebf1c18757f4be38b8993e4f0dad54e
Lauri Lubi <lauri@lubi.se>
2022-10-08 17:24:44 +0200

    DateTimeTools, TimeSpanTools

---
+++ Core/SimpleValues/DateTimeTools.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Syntactic.Sugar.Core.Exceptions;
+
+namespace Syntactic.Sugar.Core.SimpleValues;
+
+public static class DateTimeTools
+{
+    public static DateTime? GetDateOrNull(this object input)
+    {
+        if (input == null) return null;
+        if (input is DateTime inputDate) return inputDate;
+        var success = DateTime.TryParse(
+            input.ToString(),
+            CultureInfo.InvariantCulture,
+            DateTimeStyles.None,
+            out var value);
+        return success
+            ? value
+            : null;
+    }
+
+    public static DateTime GetDateOrThrow(this object input) => GetDateOrThrow(input,
+        input => new ModelValidationException($"Value {input} cannot be parsed as datetime."));
+
+    public static DateTime GetDateOrThrow<TException>(this object input, Func<object, TException> createException)
+        where TException : Exception
+    {
+        var value = GetDateOrNull(input);
+        AssertTools.Assert(value != null, () => createException(input));
+        return value.Value;
+    }
+}
---
+++ Core/SimpleValues/TimeSpanTools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Syntactic.Sugar.Core.Exceptions;
+
+namespace Syntactic.Sugar.Core.SimpleValues;
+
+public static class TimeSpanTools
+{
+    public static TimeSpan? GetTimeOrNull(this object input)
+    {
+        if (input == null) return null;
+        if (input is TimeSpan inputTime) return inputTime;
+        var success = TimeSpan.TryParse(
+            input.ToString(),
+            NumberFormatInfo.InvariantInfo,
+            out var value);
+        return success
+            ? value
+            : null;
+    }
+
+    public static TimeSpan GetTimeOrThrow(this object input) => GetTimeOrThrow(input,
+        input => new ModelValidationException($"Value {input} cannot be parsed as timespan."));
+
+    public static TimeSpan GetTimeOrThrow<TException>(this object input, Func<object, TException> createException)
+        where TException : Exception
+    {
+        var value = GetTimeOrNull(input);
+        AssertTools.Assert(value != null, () => createException(input));
+        return value.Value;
+    }
+}
