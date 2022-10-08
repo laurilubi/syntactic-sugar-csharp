58099bb3dbaea474d6a5a53ea1244e4e8c388986
Lauri Lubi <lauri@lubi.se>
2022-10-08 15:22:49 +0200

    LongTools

---
+++ Core/SimpleValues/LongTools.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Syntactic.Sugar.Core.Exceptions;
+
+namespace Syntactic.Sugar.Core.SimpleValues;
+
+public static class LongTools
+{
+    public static long? GetLongOrNull(object input)
+    {
+        if (input == null) return null;
+        if (input is long inputLong) return inputLong;
+        var success = long.TryParse(input.ToString(), out var value);
+        return success
+            ? value
+            : null;
+    }
+
+    public static long GetLongOrDefault(object input, long defaultValue = default)
+    {
+        var value = GetLongOrNull(input);
+        return value ?? defaultValue;
+    }
+
+    public static long GetLongOrThrow(object input) => GetLongOrThrow(input,
+        input => new ModelValidationException($"Value {input} cannot be parsed as long."));
+
+    public static long GetLongOrThrow<TException>(object input, Func<object, TException> createException)
+        where TException : Exception
+    {
+        var value = GetLongOrNull(input);
+        AssertTools.Assert(value != null, () => createException(input));
+        return value.Value;
+    }
+}
